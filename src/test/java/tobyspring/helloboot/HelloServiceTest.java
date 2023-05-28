08a2dabd46f53ab322624518b8791349a105c933
jinhyeong <jinhyeong.b@gmail.com>
2023-05-28 22:15:19 +0900

    섹션6. 자동 구성 기반 애플리케이션 - 메타 애노테이션과 합성 애노테이션

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -2,11 +2,26 @@ package tobyspring.helloboot;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@UnitTest
+@interface FastUnitTest{}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Test
+@interface UnitTest{}
+
 class HelloServiceTest {
-	@Test
+	@FastUnitTest
 	void simpleHelloService() {
 		// given
 		SimpleHelloService helloService = new SimpleHelloService();
