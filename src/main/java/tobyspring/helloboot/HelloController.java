22b659340997974429751a6aa001afcd04aadd5a
jinhyeong <jinhyeong.b@gmail.com>
2023-05-28 12:17:17 +0900

    섹션4. Bean의 생명주기 메소드 - 주입

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,32 +1,25 @@
 package tobyspring.helloboot;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController implements ApplicationContextAware {
+public class HelloController {
 
 	private ApplicationContext applicationContext;
 
 	private final HelloService helloService;
 
-	public HelloController(HelloService helloService) {
+	public HelloController(HelloService helloService, ApplicationContext applicationContext) {
 		this.helloService = helloService;
+		this.applicationContext = applicationContext;
 	}
 
 	@GetMapping("/hello")
 	public String hello(String name) {
 		return helloService.sayHello(Objects.requireNonNull(name));
-	}
-
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		System.out.println("applicationContext = " + applicationContext);
-		this.applicationContext = applicationContext;
 	}
 }
